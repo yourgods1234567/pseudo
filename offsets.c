becfa57e66b7c2cc01514af5ce5ce27d5bba06ef
Peter Seebach <peter.seebach@windriver.com>
2015-08-14 14:37:56 -0500

    offsets.c: #ifndef
    Turns out that we're now specifying _LARGEFILE64_SOURCE in cflags by
    default which can break the build of offsets, which #defined it
    manually. Add #ifndef.

--- offsets.c
+++ offsets.c
@@ -17,7 +17,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  *
  */
+#ifndef _LARGEFILE64_SOURCE
 #define _LARGEFILE64_SOURCE
+#endif
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
