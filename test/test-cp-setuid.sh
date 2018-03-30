f0fa9fba3c53bf4f87f33b47a583cb031b962d34
Seebs <seebs@seebs.net>
2018-03-30 16:45:19 -0500

    Fix sticky bits and a couple of test cases.
    Merge fixes from Richard Tollerton and associated test
    cases and permissions fixes.
    
    Signed-off-by: Seebs <seebs@seebs.net>

---
+++ test/test-cp-setuid.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Verify that special bits (setuid/setgid/sticky) are preserved.
+#
+# Return vals:
+#
+#                 2 - Incorrect permissions
+# All other nonzero - Unexpected command error
+#                 0 - Pass
+
+trap "rm -rf d1 d2" EXIT
+
+mkdir d1
+chmod 7777 d1
+cp -Rp d1 d2
+perms=`ls -od d1 d2 | cut -c 1-10 | uniq`
+if [ "$perms" != drwsrwsrwt ]; then
+	exit 2
+fi
+
+
+exit 0
