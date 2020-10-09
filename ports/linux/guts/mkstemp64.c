cca0d7f15b7197095cd587420d31b187620c3093
Richard Purdie <richard.purdie@linuxfoundation.org>
2020-10-09 11:46:35 +0100

    ports/linux: Add mksotemp64 wrapper
    Similar to mkstemp64 when oflags=0, therefore move the wrapper and call
    from mkstemp64. Note that some glibc versions would have one but not the other
    so ensure fall back to the real function is correct on those versions.
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- ports/linux/guts/mkstemp64.c
+++ ports/linux/guts/mkstemp64.c
@@ -8,42 +8,9 @@
  * wrap_mkstemp64(char *template) {
  *	int rc = -1;
  */
-	struct stat64 buf;
- 	int save_errno;
-	size_t len;
-	char *tmp_template;
+	/* mkstemp64() is just like mkostemp64() with no flags */
+	rc = wrap_mkostemp64(template, 0);
 
-	if (!template) {
-		errno = EFAULT;
-		return 0;
-	}
-
-	len = strlen(template);
-	tmp_template = PSEUDO_ROOT_PATH(AT_FDCWD, template, AT_SYMLINK_NOFOLLOW);
-
-	if (!tmp_template) {
-		errno = ENOENT;
-		return -1;
-	}
-
-	rc = real_mkstemp64(tmp_template);
-
-	if (rc != -1) {
-		save_errno = errno;
-
-		if (real___fxstat64(_STAT_VER, rc, &buf) != -1) {
-			real_fchmod(rc, PSEUDO_FS_MODE(0600, 0));
-			pseudo_client_op(OP_CREAT, 0, -1, -1, tmp_template, &buf);
-			pseudo_client_op(OP_OPEN, PSA_READ | PSA_WRITE, rc, -1, tmp_template, &buf);
-		} else {
-			pseudo_debug(PDBGF_CONSISTENCY, "mkstemp (fd %d) succeeded, but fstat failed (%s).\n",
-				rc, strerror(errno));
-			pseudo_client_op(OP_OPEN, PSA_READ | PSA_WRITE, rc, -1, tmp_template, 0);
-		}
-		errno = save_errno;
-	}
-	/* mkstemp only changes the XXXXXX at the end. */
-	memcpy(template + len - 6, tmp_template + strlen(tmp_template) - 6, 6);
 /*	return rc;
  * }
  */
